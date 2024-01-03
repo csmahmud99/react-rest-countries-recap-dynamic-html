2b41b944d385cdd92ad56d5f0958b5dc3b220451
csmahmud99 <csmahmud.2021@gmail.com>
2024-01-03 23:44:13 +0600

    Data Display Functions are ready

--- js/countries.js
+++ js/countries.js
@@ -5,4 +5,29 @@ const loadCountries = () => {
         .then(data => displayCountries(data));
 }
 
+
+// Display the loaded data on the project UI
+const displayCountries = countries => {
+    console.log(countries);
+    const AllCountriesHTML = countries.map(country => getCountryHTML(country));
+
+    // console.log(AllCountriesHTML);
+
+    const container = document.getElementById("countries");
+
+    // The 'join()' method is used in the following line of code is to display the countries without 'Comma Sign (,) which is found from the variable named 'AllCountriesHTML'.
+    container.innerHTML = AllCountriesHTML.join("");
+}
+
+
+const getCountryHTML = country => {
+    return `
+        <div>
+            <h2>${country.name.common}</h2>
+            <img src = ${country.flags.png} alt = "country-flags" />
+        </div>
+    `
+}
+
+
 loadCountries();
