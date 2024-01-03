ae858fb66669d74962ada7b8c62077ef8dd23968
csmahmud99 <csmahmud.2021@gmail.com>
2024-01-03 23:40:53 +0600

    Data are loaded from the external API, main function declared and called

--- js/countries.js
+++ js/countries.js
@@ -1,28 +1,8 @@
+// Load the API to the project from the global API
 const loadCountries = () => {
     fetch("https://restcountries.com/v3.1/all")
         .then(res => res.json())
         .then(data => displayCountries(data));
-}
-
-const displayCountries = countries => {
-    // console.log(countries);
-    // console.log(countries[1]);
-    const allCountriesHTML = countries.map(country => getCountryHTML(country));
-    // console.log(allCountriesHTML);
-    // console.log(allCountriesHTML[1]);
-    const container = document.getElementById("countries");
-
-    // This 'join' method is used to remove the Comma Sign (,) from the array found from the 'allCountriesHTML' variable
-    container.innerHTML = allCountriesHTML.join("");
-}
-
-const getCountryHTML = country => {
-    return `
-        <div class="country">
-            <h2>${country.name.common}</h2>
-            <img src=${country.flags.png} />
-        </div>
-    `
 }
 
 loadCountries();
