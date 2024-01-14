27939436e92692e54e5f7e3999aa82800f593514
csmahmud99 <csmahmud.2021@gmail.com>
2024-01-15 03:09:29 +0600

    Finalizing Project

--- js/countries.js
+++ js/countries.js
@@ -9,14 +9,14 @@ const loadCountries = () => {
 // Display the loaded data on the project UI
 const displayCountries = countries => {
     // console.log(countries);
-    const AllCountriesHTML = countries.map(country => getCountryHTML(country));
+    const allCountriesHTML = countries.map(country => getCountryHTML(country));
 
-    // console.log(AllCountriesHTML);
+    // console.log(allCountriesHTML);
 
     const container = document.getElementById("countries");
 
     // The 'join()' method is used in the following line of code is to display the countries without 'Comma Sign (,) which is found from the variable named 'AllCountriesHTML'.
-    container.innerHTML = AllCountriesHTML.join("");
+    container.innerHTML = allCountriesHTML.join("");
 }
 
 
